11efb1058407443fa079e51b3ab9d849f5ee322a
junxia chen <junxiachen@junxiadeMacBook-Pro.local>
2018-09-18 17:02:23 +0800

    feat: order

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,13 +9,20 @@ Page({
 
   },
   onLoad () {
+    this.isLoading = false
   },
   async uploadHandler () {
-    wx.showLoading({ title: '加载中...' })
+    if (this.isLoading) return
+    this.isLoading = true
     const res = await wxPromise.chooseImage({
       count: 1,
       sizeType: ['original']
     })
+    if (!res) {
+      this.isLoading = false
+      return
+    }
+    wx.showLoading({ title: '加载中...' })
     const filePath = res.tempFilePaths[0]
     const date = new Date()
     const name = JSON.stringify(Math.random()).substr(2)
@@ -31,12 +38,24 @@ Page({
         .add({
           data: {
             type: 0,
-            cloudPath,
-            status: 0
+            cloudPath: upload.fileID,
+            status: 0,
+            certPath: '',
+            note: '',
+            createTime: (new Date()).getTime()
           }
         })
       console.log(dbRes)
+      wx.hideLoading()
     }
-    wx.hideLoading()
+    else {
+      wx.showToast({ title: '上传失败', icon: 'none' })
+    }
+    this.isLoading = false
+  },
+  onShareAppMessage () {
+    return {
+      title: '可骄傲了'
+    }
   }
 })
--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -1,38 +1,50 @@
-// order.js
+import regeneratorRuntime from '../../utils/runtime'
+import wxPromise from '../../service/wxPromise'
+import { formatTime } from '../../utils/util'
+const db = wx.cloud.database()
+
 Page({
-  /* 页面的初始数据 */
   data: {
-
+    list: null
   },
-  /* 生命周期函数--监听页面加载 */
-  onLoad: function (options) {
-
+  onLoad () {
+    this.getOrders()
   },
-  /* 生命周期函数--监听页面初次渲染完成 */
-  onReady: function () {
-
+  async getOrders () {
+    const res = await db.collection('order')
+      .orderBy('createTime', 'desc')
+      .get()
+    console.log(res)
+    for (let i = 0; i < res.data.length; i++) {
+      res.data[i].createTime = formatTime(new Date(res.data[i].createTime))
+    }
+    this.setData({ list: res.data })
   },
-  /* 生命周期函数--监听页面显示 */
-  onShow: function () {
-
-  },
-  /* 生命周期函数--监听页面隐藏 */
-  onHide: function () {
-
-  },
-  /* 生命周期函数--监听页面卸载 */
-  onUnload: function () {
-  },
-  /* 页面相关事件处理函数--监听用户下拉动作 */
-  onPullDownRefresh: function () {
-
-  },
-  /* 页面上拉触底事件的处理函数 */
-  onReachBottom: function () {
-
-  },
-  /* 用户点击右上角分享 */
-  onShareAppMessage: function () {
-
+  async saveHandler (e) {
+    const { url } = e.target.dataset
+    if (url) {
+      // 获得图片临时链接
+      const temp = await wx.cloud.downloadFile({
+        fileID: url
+      })
+      if (!temp.tempFilePath) {
+        wxPromise.showModal({ content: temp.errMsg })
+        return
+      }
+      wx.saveImageToPhotosAlbum({
+        filePath: temp.tempFilePath,
+        success () {
+          wx.showToast({ title: '保存成功' })
+        },
+        fail (e) {
+          wxPromise.showModal({ content: e.errMsg })
+        }
+      })
+    }
+    else {
+      wxPromise.showModal({
+        content: '图片出错了，请重试或联系客服'
+      })
+    }
   }
 })
